1755fdec31522d431b6411ff8a8872070f7a4086
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-02-05 08:14:27 +0000

    chore: lint

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -1378,7 +1378,6 @@ class BaseModelSqlv2 {
       qb.limit(+rest?.limit || 25);
     }
     qb.offset(selectAllRecords ? 0 : +rest?.offset || 0);
-    
 
     const children = await this.execAndParse(qb, await childTable.getColumns());
     const proto = await (
